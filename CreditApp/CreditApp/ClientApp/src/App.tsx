2a699d4df60bcd839e7cac39a8f7fe5f3fac013a
Руслан Русланов <sharp_blade666@mail.ru>
2022-06-07 18:08:18 +0300

    form exmple

--- CreditApp/CreditApp/ClientApp/src/App.tsx
+++ CreditApp/CreditApp/ClientApp/src/App.tsx
@@ -5,16 +5,18 @@ import { Home } from './components/Home';
 
 import './custom.css'
 import {FetchData} from "./components/FetchData";
+import SendForm from "./components/SendForm";
 
-function App() {
+const App: React.FC = () => {
     return (
         <Layout>
             <Routes>
-                <Route path='/' element={<Home/>} />
+               {/* <Route path='/' element={<Home/>} />*/}
+                <Route path='/' element={<SendForm/>}/>
                 <Route path='/fetch-data' element={<FetchData/>} />
             </Routes>
         </Layout>
     );
-}
+};
 
 export default App;
